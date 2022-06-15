2873630c379717034e05dd0286c2bc3632494dd3
VisualSource <boomishere_network@outlook.com>
2022-06-15 13:11:20 -0400

    update v0.1.6

--- scripts/update.mjs
+++ scripts/update.mjs
@@ -2,41 +2,57 @@ import { exit } from 'node:process';
 import readline from 'node:readline';
 import { promisify } from 'node:util';
 import { exec } from 'child_process';
-import { readFile } from 'fs/promises';
+import { readFile, writeFile } from 'fs/promises';
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 });
-
+async function WriteJsonFile(path, content) {
+    return writeFile(path,JSON.stringify(content,undefined,2),{ encoding: "utf-8" });
+}
 async function ReadJsonFile(path) {
-    let content = readFile(path, { encoding: "utf-8" });
-    return JSON.stringify(content);
+    let content = await readFile(path, { encoding: "utf-8" });
+    return JSON.parse(content);
+}
+async function ReadTomlFile(path) {
+    let content = await readFile(path, { encoding: "utf-8" });
+    return content;
+}
+async function WriteTomlFile(path,content) {
+    return writeFile(path,content,{ encoding: "utf-8" });
 }
 
 async function main() {
-    let question = promisify(rl.question).bind(rl);
+    try {
+        let question = promisify(rl.question).bind(rl);
+        let version = await question("Enter version id. Ext 0.1.0\n");
+        if(!(/\d+.\d+.\d+/.test(version))) throw new Error("Invaild version format");
+        let areyousure = await question(`Is version ${version} Ok. (y/N)\n`);
+        if(areyousure.toLowerCase().trim() !== "y") throw new Error("Aborting update creation");
 
-    let version = await question("Enter version id. Ext 0.1.0\n");
+        const cargo_config = await ReadTomlFile("./src-tauri/Cargo.toml");
+        const tauri_config = await ReadJsonFile("./src-tauri/tauri.conf.json");
+        const npm_config = await ReadJsonFile("./package.json");
 
-    if(!(/\d+.\d+.\d+/.test(version))) throw new Error("Invaild version format");
-
-    let areyousure = await question(`Is version ${version} Ok. (y/N)\n`);
-
-    if(areyousure.toLowerCase().trim() !== "y") throw new Error("Aborting update creation");
-
-
+        const out = cargo_config.replace(/version = "\d*.\d*.\d*"/,`version = "${version}"`);
+        tauri_config.package.version = version;
+        npm_config.version = version;
 
 
+        await WriteJsonFile("./src-tauri/tauri.conf.json",tauri_config);
+        await WriteJsonFile("./package.json",npm_config);
+        await WriteTomlFile("./src-tauri/Cargo.toml",out);
 
 
-
-    rl.close();
+        return 0;
+    } catch (error) {
+        console.log(error.message);
+        return 1;
+    }
 }
-
-
-main().catch((err)=>{ 
-    console.error(err.message);
-    exit(1);
-});
+main().then((code)=>{
+    rl.close();
+    exit(code);
+}); 
 
