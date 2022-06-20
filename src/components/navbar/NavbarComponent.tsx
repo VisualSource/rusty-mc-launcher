720d51c256e5273b3f7f4d3a29adac505818a032
VisualSource <boomishere_network@outlook.com>
2022-06-19 23:16:14 -0400

    remove force login

--- src/components/navbar/NavbarComponent.tsx
+++ src/components/navbar/NavbarComponent.tsx
@@ -19,7 +19,6 @@ export default function NavbarComponent() {
     const USER_MENU = useMemo(()=>(
         <Menu>
           { user.active ? <MenuItem text="Logout" onClick={()=>user.logout()}/> : <MenuItem text="Login" onClick={()=>user.login()}/> }
-          <MenuItem text="Force Logout" onClick={()=>user.logout()}/>
         </Menu>
     ),[user]);
     const LAUNCHER_MENU = useMemo(()=>(
