12d0157920aa408e41170e98bbd9f8deeddb83eb
VisualSource <boomishere_network@outlook.com>
2022-06-25 20:00:16 -0400

    updated mod list

--- src/components/navbar/NavbarComponent.tsx
+++ src/components/navbar/NavbarComponent.tsx
@@ -1,12 +1,24 @@
-import { Button, AnchorButton, Menu, MenuItem, MenuDivider, Classes } from "@blueprintjs/core";
+import { Button, AnchorButton, Menu, MenuItem, MenuDivider, Classes, Intent } from "@blueprintjs/core";
+import { useNotificationCenter } from "react-toastify/addons/use-notification-center";
 import { useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Popover2 } from '@blueprintjs/popover2';
 import { appWindow } from '@tauri-apps/api/window';
 import { useUser } from "../account/Account";
 import css from './navbar.module.sass';
+
+const GetIntent = {
+  'info': Intent.PRIMARY,
+  'success': Intent.SUCCESS,
+  'warning': Intent.WARNING,
+  'error': Intent.DANGER,
+  'default': Intent.NONE,
+}
+
 // @see https://docs.microsoft.com/en-us/graph/api/profilephoto-get?view=graph-rest-1.0
 export default function NavbarComponent() {
+    //@ts-ignore
+    const { unreadCount, notifications, markAsRead } = useNotificationCenter({ filter: (item) => !item.read  });
     const user = useUser();
     const navigate = useNavigate();
     const LIBRARY_MENU = useMemo(()=>(
@@ -32,6 +44,7 @@ export default function NavbarComponent() {
           <MenuItem text="Report Issue" href="https://github.com/VisualSource/rusty-mc-launcher/issues" target="_blank"/>
       </Menu>
     ),[]);
+    
     return (
         <nav data-tauri-drag-region className={css.navbar}>
           <div data-tauri-drag-region>
@@ -54,6 +67,19 @@ export default function NavbarComponent() {
           </div>
           <div data-tauri-drag-region className={css.navbar_left}>
                 <div className={css.user}> 
+                  <Popover2 modifiers={{ arrow: { enabled: false } }} content={(
+                    <Menu>
+                        { notifications.length === 0 ? (<MenuItem text="No Notifications"/>) : notifications.map((item,i)=>(
+                          <MenuItem key={i} text={item.content} intent={GetIntent[item.type ?? "default"]} onClick={()=>markAsRead(item.id)}>
+                          
+                          </MenuItem>
+                        ))}
+                    </Menu>
+                  )}>
+                    <Button icon="notifications">
+                        { unreadCount }
+                    </Button>
+                  </Popover2>
                   <img alt="user profile" src="/images/Vanilla.webp"/>
                   <Popover2 modifiers={{ arrow: { enabled: false } }} content={USER_MENU} placement="bottom-end">
                     <Button className={user.loading ? Classes.SKELETON: ""} text={user.profile?.profile.name ?? "LOGIN"} rightIcon="caret-down"/>
