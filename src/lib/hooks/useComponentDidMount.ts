647d3f6de22219bf47d5cdf0e998c4797e516b86
VisualSource <boomishere_network@outlook.com>
2022-06-25 20:37:42 -0400

    fix warnings

--- src/lib/hooks/useComponentDidMount.ts
+++ src/lib/hooks/useComponentDidMount.ts
@@ -2,14 +2,14 @@ import { useEffect, useRef, useCallback } from "react";
 
 const useComponentDidMount = (cb: ()=> Promise<void>): void => {
     const didMount = useRef(false);
-    const callback = useCallback(cb,[]);
+    const callback = useCallback(cb,[cb]);
 
     useEffect(()=>{
         if(!didMount.current) {
             callback();
             didMount.current = true;
         }
-    },[]);
+    },[callback]);
 }
 
 export default useComponentDidMount;
