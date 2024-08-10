108a3689113a2d29364ad5702933314eff942992
VisualSource <collin_blosser@yahoo.com>
2024-08-10 16:01:00 -0400

    bun like query

--- src/lib/api/plugins/query.ts
+++ src/lib/api/plugins/query.ts
@@ -4,23 +4,22 @@ import { invoke } from "@tauri-apps/api/core";
 // const result = await invoke<unknown[]>("plugin:rmcl-query|execute",{ query });
 
 type Query<T> = {
-    all: () => Promise<T>
-    get: () => Promise<T>
+    all: () => Promise<T[]>
+    get: () => Promise<T | undefined>
     run: () => Promise<void>,
     as: <A>(model: { new(...args: unknown[]): A }) => Omit<Query<A>, "as" | "run">
 }
 
-export function query<T>(stmt: string): Query<T> {
+export function query<T>(stmt: string, args: unknown[]): Query<T> {
 
     return {
         async all() {
-            const result = await invoke<T>("plugin:rmcl-query|execute", { query: stmt });
+            const result = await invoke<T[]>("plugin:rmcl-query|execute", { query: stmt, args });
             return result;
         },
         async get() {
-            const result = await invoke<T>("plugin:rmcl-query|execute", { query: stmt });
-
-            return result;
+            const result = await invoke<T[]>("plugin:rmcl-query|execute", { query: stmt, args });
+            return result.at(0);
         },
         async run() {
 
@@ -28,11 +27,15 @@ export function query<T>(stmt: string): Query<T> {
         as<A>(Model: { new(...args: unknown[]): A }) {
             return {
                 async all() {
-                    const result = await invoke<unknown>("plugin:rmcl-query|execute", { query: stmt });
-                    return new Model();
+                    const result = await invoke<unknown[]>("plugin:rmcl-query|execute", { query: stmt, args });
+                    return result.map(() => new Model());
                 },
                 async get() {
-                    const result = await invoke<unknown[]>("plugin:rmcl-query|execute", { query: stmt });
+                    const result = await invoke<unknown[]>("plugin:rmcl-query|execute", { query: stmt, args });
+
+                    const value = result.at(0)
+                    if (!value) return undefined;
+
                     return new Model(...result)
                 },
             }
