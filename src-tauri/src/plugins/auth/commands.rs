059d76c1319662ea7fa3e996154b85846c71af09
VisualSource <collin_blosser@yahoo.com>
2024-09-05 19:08:51 -0400

    fix command bug

--- src-tauri/src/plugins/auth/commands.rs
+++ src-tauri/src/plugins/auth/commands.rs
@@ -1,18 +1,17 @@
 use super::desktop::{cancel, start};
 use crate::error::Result;
 use log::{debug, error};
+use tauri::Emitter;
 
 #[tauri::command]
-pub fn start_auth_server(app: tauri::AppHandle) -> Result<u16> {
-    /*
-      let port = start(move |url| {
+pub fn start_auth_server<R: tauri::Runtime>(app: tauri::AppHandle<R>) -> Result<u16> {
+    let port = start(move |url| {
         if let Err(err) = app.emit("rmcl://auth_response", url) {
             error!("{}", err);
         }
     })?;
-     */
-    //debug!("Starting Auth Server on port: {}", port);
-    Ok(2000)
+    debug!("Starting Auth Server on port: {}", port);
+    Ok(port)
 }
 
 #[tauri::command]
