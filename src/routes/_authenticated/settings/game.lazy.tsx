847eadcbc056d5703f3e555c0412e8d47e5280d0
VisualSource <collin_blosser@yahoo.com>
2025-06-05 18:27:27 -0400

    add config laucher wait time

--- src/routes/_authenticated/settings/game.lazy.tsx
+++ src/routes/_authenticated/settings/game.lazy.tsx
@@ -1,3 +1,4 @@
+import { millisecondsToMinutes, minutesToMilliseconds } from "date-fns";
 import { createLazyFileRoute } from "@tanstack/react-router";
 import { open } from "@tauri-apps/plugin-dialog";
 import { CopyX, FolderCog } from "lucide-react";
@@ -13,18 +14,19 @@ import {
 	FormLabel,
 	FormMessage,
 } from "@/components/ui/form";
-import { Separator } from "@/components/ui/separator";
 import { isOption, getConfig, upsert } from "@/lib/models/settings";
+import { Separator } from "@/components/ui/separator";
 import { Button } from "@/components/ui/button";
 import { Switch } from "@/components/ui/switch";
 import { Loading } from "@/components/Loading";
 import { Input } from "@/components/ui/input";
 import { toastSuccess } from "@/lib/toast";
 
-type State = { exitOnStart: boolean; copyOptionsFrom?: string };
+type State = { exitOnStart: boolean; copyOptionsFrom?: string, maxLaunchWait?: number };
 
 const OPTION_EXIT_ON_START = "option.exit_on_start";
 const OPTION_COPY_SETTINGS = "option.copy_settings_from";
+const OPTION_MAX_LAUNCH_WAIT = "option.max_launch_wait";
 
 const selectFile = async () => {
 	const selected = await open({
@@ -54,12 +56,17 @@ function GameSettings() {
 	const form = useForm<State>({
 		async defaultValues() {
 			try {
-				const [exitOnStart, copyOptionsFrom] = await Promise.all([
+				const [exitOnStart, copyOptionsFrom, maxLaunchWait] = await Promise.all([
 					isOption(OPTION_EXIT_ON_START, "TRUE"),
 					getConfig(OPTION_COPY_SETTINGS).then((e) => e?.value),
+					getConfig(OPTION_MAX_LAUNCH_WAIT).then(e => {
+						if (!e?.value) return;
+						const ticks = Number.parseInt(e.value);
+						return millisecondsToMinutes(ticks * 250);
+					})
 				]);
 
-				return { exitOnStart, copyOptionsFrom };
+				return { exitOnStart, copyOptionsFrom, maxLaunchWait };
 			} catch (error) {
 				return { exitOnStart: false };
 			}
@@ -68,7 +75,15 @@ function GameSettings() {
 
 	const onSubmit = async (state: State) => {
 		await upsert(OPTION_EXIT_ON_START, state.exitOnStart ? "TRUE" : "FALSE");
-		await upsert(OPTION_COPY_SETTINGS, state.copyOptionsFrom ?? "");
+
+		if (state.copyOptionsFrom) {
+			await upsert(OPTION_COPY_SETTINGS, state.copyOptionsFrom);
+		}
+
+		if (state.maxLaunchWait) {
+			const ticks = minutesToMilliseconds(state.maxLaunchWait) / 250;
+			await upsert(OPTION_MAX_LAUNCH_WAIT, ticks);
+		}
 
 		toastSuccess({ title: "Settings Saved" });
 	};
@@ -84,40 +99,18 @@ function GameSettings() {
 				<Form {...form}>
 					<form
 						onSubmit={form.handleSubmit(onSubmit)}
-						className="mt-4 space-y-2"
+						className="mt-4 space-y-4"
 					>
 						<FormField
-							control={form.control}
-							name="exitOnStart"
-							render={({ field }) => (
-								<FormItem>
-									<div className="flex flex-row-reverse items-center justify-end gap-2">
-										<FormLabel>Exit on game start</FormLabel>
-										<FormControl>
-											<Switch
-												checked={field.value}
-												onCheckedChange={(ev) => field.onChange(ev)}
-											/>
-										</FormControl>
-									</div>
-									<FormDescription>
-										Control if the launcher exits on game start.
-									</FormDescription>
-									<FormMessage />
-								</FormItem>
-							)}
-						/>
-
-						<FormField
-							control={form.control}
 							name="copyOptionsFrom"
+							control={form.control}
 							render={({ field }) => (
 								<FormItem>
 									<div className="flex-col gap-2 flex">
 										<FormLabel>Copy Options From</FormLabel>
 										<FormControl>
 											<div className="flex gap-2">
-												<Input readOnly value={field.value} />
+												<Input placeholder="C:\.minecraft\options.txt" readOnly value={field.value} />
 												<Button
 													title="Open"
 													type="button"
@@ -148,6 +141,44 @@ function GameSettings() {
 								</FormItem>
 							)}
 						/>
+
+						<FormField
+							name="exitOnStart"
+							control={form.control}
+							render={({ field }) => (
+								<FormItem>
+									<div className="flex flex-row-reverse items-center justify-end gap-2">
+										<FormLabel>Exit on game start</FormLabel>
+										<FormControl>
+											<Switch
+												checked={field.value}
+												onCheckedChange={(ev) => field.onChange(ev)}
+											/>
+										</FormControl>
+									</div>
+									<FormDescription>
+										Control if the launcher exits on game start.
+									</FormDescription>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+
+						<FormField name="maxLaunchWait" control={form.control} rules={{
+							min: { message: "Min wait time is a 1min", value: 1 },
+							max: { message: "Outside the max wait time", value: 30 },
+						}} render={({ field }) => (
+							<FormItem>
+								<FormLabel>Max Launch Wait</FormLabel>
+								<FormControl>
+									<Input placeholder="1 minute" {...field} type="number" step="1" />
+								</FormControl>
+								<FormDescription>
+									The max amount of time the launcher will wait for minecraft to launch. (Default 1 minute)
+								</FormDescription>
+								<FormMessage />
+							</FormItem>
+						)} />
 
 						<div className="flex w-full justify-end">
 							<Button type="submit">Save</Button>
