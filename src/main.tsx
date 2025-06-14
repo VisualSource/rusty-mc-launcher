99f46672d529c6e8ece36a1579f2cf2e14999346
VisualSource <collin_blosser@yahoo.com>
2025-06-14 17:44:22 -0400

    fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
+import { getTauriVersion, getVersion } from "@tauri-apps/api/app";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "@tanstack/react-router";
+import { error, info } from "@tauri-apps/plugin-log";
 import { listen } from "@tauri-apps/api/event";
 import { MsalProvider } from "@azure/msal-react";
 import { createRoot } from "react-dom/client";
@@ -10,18 +12,16 @@ import '@fontsource-variable/geist-mono';
 
 import { ModrinthProvider } from "./components/providers/ModrinthProvider";
 import { ProcessProvider } from "@component/providers/ProcessProvider";
+import { ModrinthClientApplication } from "./lib/auth/modrinth";
 import { checkForAppUpdate } from "./lib/system/updateCheck";
 import { queryClient } from "@lib/api/queryClient";
 import { logCatchError } from "./lib/catchError";
+import { initThemes } from "./lib/api/themes";
+import { toastError } from "./lib/toast";
 import { getPCA } from "@auth/msal";
 import { router } from "./router";
 import "./index.css";
 import "react-lazy-load-image-component/src/effects/blur.css";
-import { toastError } from "./lib/toast";
-import { initThemes } from "./lib/api/themes";
-import { getTauriVersion, getVersion } from "@tauri-apps/api/app";
-import { error, info } from "@tauri-apps/plugin-log";
-import { ModrinthClientApplication } from "./lib/auth/modrinth";
 
 initThemes();
 if (import.meta.env.PROD) checkForAppUpdate().catch(logCatchError);
@@ -30,6 +30,7 @@ Promise.all([getTauriVersion(), getVersion()]).then(([tauri, app]) => {
 	Shake.config.floatingButtonEnabled = false;
 	Shake.report.screenshotIncluded = false;
 	Shake.report.isSessionReplayEnabled = false;
+
 	Shake.setMetadata("app_version", app);
 	Shake.setMetadata("tauri", tauri);
 	Shake.start(import.meta.env.VITE_SHAKE_APIKEY)
